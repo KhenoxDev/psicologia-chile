a27dd16746c4b0a11cd9dbaeca53018029e53b54
Jean Rodriguez <jean.rodriguezr@gmail.com>
2020-06-22 12:18:27 -0400

    Implementación de Vue y creación del home

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="demo" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+      <li data-target="#demo" data-slide-to="3"></li>
+    </ul>
+
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/banner-1.jpg" alt="Banner 1" />
+      </div>
+      <div class="carousel-item">
+        <img src="img/banner-2.jpg" alt="Banner 2" />
+      </div>
+      <div class="carousel-item">
+        <img src="img/banner-3.jpg" alt="Banner 3" />
+      </div>
+      <div class="carousel-item">
+        <img src="img/banner-4.jpg" alt="Banner 4" />
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>
+</template>
+<style lang="scss">
+.carousel-inner img {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-white text-uppercase" id="mainNav">
+    <div class="container-fluid">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <img class="logo" :src="logo" :alt="logo-txt" />
+      </a>
+      <button
+        class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-0 mx-lg-1">
+            <a
+              class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+              href="#portfolio"
+            >¿Quienes somos?</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a
+              class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+              href="#portfolio"
+            >¿Cómo funciona?</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a
+              class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+              href="#portfolio"
+            >Profesionales</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a
+              class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+              href="#portfolio"
+            >Especialidades</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Planes</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a
+              class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+              href="#portfolio"
+            >Previsión</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1 login--container">
+            <a href class>Registrate</a>
+            <a href class>Iniciar sesión</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  props: ["logo", "logo-txt"]
+};
+</script>
+<style lang="scss">
+.logo {
+  min-width: 25%;
+  max-width: 80%;
+  width: 100%;
+}
+
+.login--container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  a {
+    margin-left: 1rem;
+    padding: 1rem;
+    background-color: #f7941e;
+    border-radius: 15px;
+    font-size: 0.9rem;
+    text-decoration: none;
+    color: #fff;
+  }
+}
+</style>
+
