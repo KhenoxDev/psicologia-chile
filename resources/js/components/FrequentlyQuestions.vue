e26b51e0335dce9d66324a02b21891fefecde9d9
Alvaro Musa <alvaromusap@gmail.com>
2020-07-01 19:12:30 -0400

    Se diferencia diseño de primera tarjeta con el resto.

--- resources/js/components/FrequentlyQuestions.vue
+++ resources/js/components/FrequentlyQuestions.vue
@@ -7,25 +7,34 @@
       :bullets="false"
       :autoplay="true"
       :arrows="false"
-      :infinite="true"
+      :infinite="false"
       :transitionSpeed="100"
       :visible-slides="4"
       :slide-ratio="1 / 8"
       :dragging-distance="70"
     >
-      <vueper-slide v-for="slide in slides" :key="slide.id" :style="'margin-bottom:' + ['1rem']">
-        <template v-slot:content>
-          <div class="card card-body carousel--prevision first-card-faq">
-            <img class="img-fluid" src="img/faq_interrogacion_primera_pregunta5.svg" alt />
-            <div class="texto-encima33">
-              Tengo crisis de pánico muy seguido
-              <br />¿qué puedo hacer?
-            </div>
-            <div class></div>
-          </div>
-        </template>
+       <vueper-slide v-for="(slide, i) in slides" :index="i" :key="slide.id" class="test--margin">
+       <template v-slot:content>
+		<!-- Primera tarjeta pintada  -->
+		<div v-if="i == 0" class="card card-body carousel--prevision first-card-faq border-frequentlyquestions">
+			<img class="img-fluid" src="img/faq_interrogacion_primera_pregunta.svg" alt />
+			<div class="texto-first-frequentlyquestions">
+				{{ slide.descFAQ }}
+			</div>
+		</div>
+		<!-- Resto de tarjetas -->
+		<div v-else class="card card-body carousel--prevision another-card-faq border-frequentlyquestions">
+			<div class="texto-another-frequentlyquestions">
+				{{ slide.descFAQ }}
+			</div>
+		</div>
+		<!-- Fin sentencia if -->
+	</template>
       </vueper-slide>
     </vueper-slides>
+	<div class="seemore--questions">
+		<button>ver todas las consultas</button>
+	</div>
   </div>
 </template>
 <script>
@@ -38,31 +47,45 @@ export default {
       slides: [
         {
           title: "Slide #1",
-          content: "Slide content."
+          content: "Slide content.",
+		  descFAQ: "Tengo crisis de pánico muy seguido ¿qué puedo hacer?",
+		  position: "1"
         },
         {
           title: "Slide #2",
-          content: "Slide content."
+		  content: "Slide content.",
+		  descFAQ: "¿Qué es la terapia psicológica online?",
+		  position: "2"
         },
         {
           title: "Slide #3",
-          content: "Slide content."
+		  content: "Slide content.",
+		  descFAQ: "Cómo puedo superar mis miedos, aceptarme y enfrentar la vida",
+		  position: "3"
         },
         {
           title: "Slide #4",
-          content: "Slide content."
+		  content: "Slide content.",
+		  descFAQ: "¿Cuál es la principal diferencia de la terapia online, versus la terapia psicológica tradicional?",
+		  position: "4"
         },
         {
           title: "Slide #5",
-          content: "Slide content."
+		  content: "Slide content.",
+		  descFAQ: "¿Por qué es más económica?",
+		  position: "5"
         },
         {
           title: "Slide #6",
-          content: "Slide content."
+		  content: "Slide content.",
+		  descFAQ: "¿Cómo es la confidencialidad de la terapia?",
+		  position: "6"
         },
         {
           title: "Slide #7",
-          content: "Slide content."
+		  content: "Slide content.",
+		  descFAQ: "¿Cómo es el proceso de selección de nuestros profesionales?",
+		  position: "7"
         }
       ]
     };
