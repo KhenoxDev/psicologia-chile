c4d5a5f6df90b5b7e194f61fc22e62fce7bd107b
Alvaro Musa <alvaromusap@gmail.com>
2020-08-07 00:32:44 -0400

    Se avanza estilo seccion profesional detalle.

--- resources/js/components/Professional/DetailProfessional.vue
+++ resources/js/components/Professional/DetailProfessional.vue
@@ -11,10 +11,58 @@
     <div class="bg--section bg--professional-banner">
       <banner
         banner-identification="mainProfessional--banner"
-        :banner-title="professionals[0].nombre"
+        :banner-title="this.professionals[0].nombreCompleto"
       ></banner>
     </div>
     <!-- Acá va tu código de diseño recorrer professionals-->
+    <div id="detailProfessional--section" class="bg--white">
+      <div class="detailProfessional--content">
+        <div class="detailImage">
+          <img :src="this.professionals[0].foto" alt />
+        </div>
+
+        <div class="detailText">
+          <span class="span--fullname">{{ this.professionals[0].nombreCompleto }}</span>
+          <span class="span--profesion" v-html="this.professionals[0].profesion"></span>
+          <span class="span--codigo">
+            <strong>Código:</strong>
+            {{ this.professionals[0].codigo }}
+          </span>
+          <div class="description--container">
+            <p class="p--descripcion" v-html="this.professionals[0].descripcion"></p>
+          </div>
+          <div class="specialist--container">
+            <span class="specialist--title">Especialidades:</span>
+            <span
+              class="badge badge--psicologia span--specialist"
+              v-for="spec in this.professionals[0].especialidades"
+              :key="spec.id"
+            >{{ spec }}</span>
+          </div>
+
+          <div class="prevision--container">
+            <span class="prevision--title">Previsiones:</span>
+            <span
+              class="badge badge--psicologia span--prevision"
+              v-for="prev in this.professionals[0].prevision"
+              :key="prev.id"
+            >{{ prev }}</span>
+          </div>
+          <div class="schedule--container">
+            <span class="schedule--title">Días de atención:</span>
+            <span
+              class="span--schedule badge badge--schedule"
+            >{{ this.professionals[0].diasAtencion }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="detailPlan--section" class="bg--white">
+      <span class="planSection--title">Planes y valores</span>
+      <hr />
+      <span class="span--plan" v-for="pln in this.professionals[0].plan" :key="pln.id">{{ pln }}</span>
+    </div>
   </div>
 </template>
 <script>
