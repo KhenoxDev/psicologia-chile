e7063db10b51d9fa5b26822285d5024b24f82719
Jean <jean.rodriguezr@gmail.com>
2020-08-14 17:50:35 -0400

    Avance

--- resources/js/components/Specialist/Main.vue
+++ resources/js/components/Specialist/Main.vue
@@ -18,9 +18,40 @@
             :title="specialist.nombre"
             :id="specialist.codigo"
             href="/acoso-laboral"
+            data-toggle="modal"
+            data-target="#exampleModal"
+            @click="getProfessionalsBySpecialist(specialist.codigo)"
           >{{ specialist.nombre }}</a>
         </li>
       </ul>
+    </div>
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="specialistLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="specialistLabel">{{ specialistName }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            {{ specialistDescription }}
+            <ul>
+              <li v-for="prof in specialistProfessionals" :key="prof.id">{{ prof.nombreCompleto }}</li>
+            </ul>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +66,16 @@ export default {
       isLoading: true,
       fullPage: true,
       specialists: [],
+      specialistProvisional: [
+        {
+          name: "Estrés",
+          description:
+            "El estrés es la reacción de su cuerpo a un desafío o demanda. En pequeños episodios el estrés puede ser positivo, como cuando le ayuda a evitar el peligro o cumplir con una fecha límite. Pero cuando el estrés dura mucho tiempo, puede dañar su salud.",
+        },
+      ],
+      specialistName: "sdadas",
+      specialistDescription: "dsadsa",
+      specialistProfessionals: [],
     };
   },
   components: {
@@ -59,9 +100,25 @@ export default {
 
       try {
         let response = await axios.get(api);
-        console.log(response.data);
+
         for (let index = 0; index < response.data.items.length; index++) {
           this.specialists.push(response.data.items[index]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getProfessionalsBySpecialist(id) {
+      this.specialistProfessionals = [];
+      const api =
+        "https://online.psicologiachile.cl/gateway-json.php?service=staff&especialidad=" +
+        id;
+
+      try {
+        let response = await axios.get(api);
+
+        for (let index = 0; index < response.data.items.length; index++) {
+          this.specialistProfessionals.push(response.data.items[index]);
         }
       } catch (error) {
         console.log(error);
