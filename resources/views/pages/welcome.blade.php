630eef8dbf981cb20beeb7875daf44d034d6dead
Jean Rodríguez <jean.rodriguezr@gmail.com>
2020-08-19 15:13:35 -0400

    Burbuja en banner principal

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,5 +3,5 @@
 @section('title', 'Psicología Chile')
 
 @section('content')
-	<home-component></home-component>
+	<home-component img-background="{{ asset('img/banner/terapiaonline.jpg') }}"></home-component>
 @endsection
