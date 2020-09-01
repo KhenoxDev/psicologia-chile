a640d7db39240bb95a53b704e6595fa2e877b6e3
Jean Rodríguez <jean.rodriguezr@gmail.com>
2020-09-01 15:21:55 -0400

    Se arregla degradado fatal según Andrea

--- resources/js/components/Home/Specialists.vue
+++ resources/js/components/Home/Specialists.vue
@@ -10,7 +10,7 @@
           v-for="item in col1"
           :key="item.id"
           :class="'card--specialist ' + item.class"
-          :style="'background-image: linear-gradient(to left, white 30%, transparent 80%), url(' + item.image + ');'"
+          :style="'background-image: linear-gradient(' + item.colour + ', ' + item.colour + '), url(' + item.image + ');'"
           :href="'profesionales/spec/' + item.title"
           target="_blank"
         >
@@ -24,7 +24,7 @@
           v-for="item in col2"
           :key="item.id"
           :class="'card--specialist ' + item.class"
-          :style="'background-image: linear-gradient(to left, white 30%, transparent 80%), url(' + item.image + ');'"
+          :style="'background-image: linear-gradient(' + item.colour + ', ' + item.colour + '), url(' + item.image + ');'"
         >
           <div class="specialist">
             <span class="title--psicologia">{{ item.title }}</span>
@@ -41,7 +41,7 @@
           v-for="item in col3"
           :key="item.id"
           :class="'card--specialist ' + item.class"
-          :style="'background-image: linear-gradient(to left, white 30%, transparent 80%), url(' + item.image + ');'"
+          :style="'background-image: linear-gradient(' + item.colour + ', ' + item.colour + '), url(' + item.image + ');'"
           :href="'profesionales/spec/' + item.title"
           target="_blank"
         >
@@ -62,16 +62,19 @@ export default {
           title: "Adicciones",
           image: "img/specialists/adicciones.jpg",
           class: "",
+          colour: "rgba(120, 87, 102, 0.7)",
         },
         {
           title: "Bullying",
           image: "img/specialists/bullying.jpg",
           class: "",
+          colour: "rgba(142, 102, 188, 0.7)",
         },
         {
           title: "Estrés",
           image: "img/specialists/estres.jpg",
           class: "",
+          colour: "rgba(215, 124, 148, 0.7)",
         },
       ],
       col2: [
@@ -79,6 +82,7 @@ export default {
           title: "Psicología Online",
           image: "img/specialists/psicologia.jpg",
           class: "card--specialist-max",
+          colour: "rgba(144, 98, 88, 0.7)",
         },
       ],
       col3: [
@@ -86,16 +90,19 @@ export default {
           title: "Ruptura Emocional",
           image: "img/specialists/ruptura.jpg",
           class: "",
+          colour: "rgba(129, 99, 101, 0.7)",
         },
         {
           title: "Depresión",
           image: "img/specialists/depresion.jpg",
           class: "",
+          colour: "rgba(215, 124, 148, 0.7)",
         },
         {
           title: "Terapia de Parejas",
           image: "img/specialists/parejas.jpg",
           class: "",
+          colour: "rgba(142, 102, 188, 0.7)",
         },
       ],
     };
