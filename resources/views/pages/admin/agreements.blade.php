3199775c6078565cccd0cbe4ca88dec4c8eacfbc
Jean Rodriguez <jean.rodriguezr@gmail.com>
2020-09-29 22:26:32 -0300

    lista de psicologos

--- resources/views/pages/admin/agreements.blade.php
+++ resources/views/pages/admin/agreements.blade.php
@@ -29,9 +29,9 @@
                         <td>{!! $agreement->conditions !!}</td>
                         <td> <a href="{{ asset($agreement->doc) }}" target="_blank">Ver documento</a></td>
                         <td>
-                           
                             <a href="{{ route('admin.destroy.agreements', $agreement->id) }}"><i
                                     class="fas fa-times"></i></a>
+                            <a href="#" class="link-btn" data-id="{{ $agreement->id }}"><i class="fas fa-link"></i></a>
                         </td>
                     </tr>
                 @endforeach
@@ -47,7 +47,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="{{ route('admin.store.agreements') }}" method="POST"  enctype="multipart/form-data">
+                <form action="{{ route('admin.store.agreements') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="modal-body">
                         <div class="form-group">
@@ -58,8 +58,8 @@
                         <div class="form-group">
                             <label for="descripcion">{{ __('Descripción *') }}</label>
                             <textarea id="descripcion" name="descripcion" cols="30" rows="10"></textarea>
-						</div>
-						
+                        </div>
+
 
                         <div class="form-group">
                             <label for="condicion">{{ __('Condición *') }}</label>
@@ -76,8 +76,8 @@
                         <div class="form-group">
                             <label for="imagen">{{ __('Imagen *') }}</label>
                             <input type="file" class="form-control" id="imagen" name="imagen">
-						</div>
-					
+                        </div>
+
 
                     </div>
                     <div class="modal-footer">
@@ -85,6 +85,27 @@
                         <button type="submit" class="btn btn-primary">Guardar</button>
                     </div>
                 </form>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="linkModal" tabindex="-1" aria-labelledby="linkModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="linkModalLabel">Vincular psicólogos </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div id="staff" class="form-group staff-list">
+
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
             </div>
         </div>
     </div>
@@ -111,14 +132,14 @@
                 .create(document.querySelector('#descripcion'))
                 .catch(error => {
                     console.error(error);
-				});
+                });
 
-			ClassicEditor
+            ClassicEditor
                 .create(document.querySelector('#condicion'))
                 .catch(error => {
                     console.error(error);
-				});
-				
+                });
+
 
             $('#questions').DataTable({
                 "paging": true,
@@ -148,6 +169,46 @@
                         "sortDescending": ": activate to sort column descending"
                     },
                 }
+            });
+
+            $('.link-btn').click(function() {
+                var id = $(this).data('id');
+                var url = "https://online.psicologiachile.cl/gateway-json.php?service=staff";
+
+                $.ajax({
+                    type: 'get',
+                    url: url,
+                    success: function(response) {
+                        let data = JSON.parse(response);
+                        console.log(data.items);
+                        $("#staff").empty();
+                        data.items.forEach(element => {
+                            $("#staff").append(`<div class="form-check form-check-inline">
+                                   <input class="form-check-input" type="checkbox" id="${element.index}" value="${element.index}">
+                                   <label class="form-check-label" for="${element.index}">${element.nombreCompleto}</label>
+                                   </div>`);
+                        });
+                        // $('#id_edit').val(data[0].id);
+                        // $('#rut_edit').val(data[0].rut);
+                        // $('#name_edit').val(data[0].name);
+                        // $('#last_name_edit').val(data[0].last_name);
+                        // $('#rol_id_edit').empty();
+                        // $.each(data[1], function(i, item) {
+                        //     if (data[0].rol_id == item.id) {
+                        //         $("#rol_id_edit").append("<option value='" +
+                        //             item.id +
+                        //             "' selected>" + item.name +
+                        //             "</option>")
+
+                        //     } else {
+                        //         $("#rol_id_edit").append("<option value='" +
+                        //             item.id +
+                        //             "'>" + item.name + "</option>")
+                        //     }
+                        // });
+                        $('#linkModal').modal('show');
+                    }
+                });
             });
         });
 
